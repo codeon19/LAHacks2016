2fcf8eaa9115e98cc63cc552f76deacae10ba7fd
teamguy <bill.breck@gmail.com>
2016-04-30 22:04:05 -0400

    color animation

--- script.js
+++ script.js
@@ -13,5 +13,20 @@ var main= function() {
     });
 }
 
+var evenOrOdd = 0;
+
+$('form').submit(function() {
+    
+    if(evenOrOdd % 2 == 0){
+        $('#something').animate({ backgroundColor: "red" }, 1000);
+    }
+    else{
+        $('#something').animate({ backgroundColor: "green" }, 1000);
+    }
+    
+    evenOrOdd++;
+    
+});
+
 <!-- When the document is ready, call it --> 
 $(document).ready(main);
